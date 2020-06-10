514fb64826233e939a59c09619fd0b62c90b850b
poornithsundar <40591754+poornithsundar@users.noreply.github.com>
2020-06-10 10:29:18 +0530

    Validate for lists Of lists for Optional Params

--- src/com/Validate_Params.java
+++ src/com/Validate_Params.java
@@ -5,42 +5,57 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Validate_Params {
-	static void doValidateParams(List<String> requestParams, List<String> mandatoryParams, List<String> optionalParams, List<String> semiOptionalParams)
+	static void doSemiValidateParams(List<String> requestParams, List<List<String>> semiOptionalParamsList)
 	{
-		int c = 0;
-		for(String i:requestParams)
+		int count=0;
+		for(int j=0;j<semiOptionalParamsList.size();j++)
 		{
-			if(semiOptionalParams.contains(i))
+			List<String> semiOptionalParams = semiOptionalParamsList.get(j);
+			int c = 0;
+			for(String i:requestParams)
 			{
-				c+=1;
+				if(semiOptionalParams.contains(i))
+				{
+					c+=1;
+				}
+			}
+			if(c==0)
+			{
+				System.out.println("Atleast one of the optional params are required.....!"+semiOptionalParams.toString());
+			}
+			else if(c==semiOptionalParams.size())
+			{
+				System.out.println("All the optional params should not be given .....!"+semiOptionalParams.toString());
+			}
+			else
+			{
+				count++;
 			}
 		}
-		if(c==0)
+		if(count==semiOptionalParamsList.size())
 		{
-			System.out.println("Atleast one of the optional params are required.....!");
-		}
-		else if(c==semiOptionalParams.size())
-		{
-			System.out.println("All the optional params should not be given .....!");
-		}
-		else
-		{
-			System.out.println("Process request.....");
+			System.out.println("Process the Request");
 		}
 	}
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		List<String> request = new ArrayList<>();
-		List<String> mandatory = new ArrayList<>();
-		List<String> optional = new ArrayList<>();
-		List<String> semiOptional = new ArrayList<>();
-		semiOptional.add("phone");
-		semiOptional.add("email");
-		System.out.println("What are the optional params given in request?(in single line with spaces)");
+		List<List<String>> semiOptional = new ArrayList<>();
+		System.out.println("Enter the number of semi optional params?");
+		int n = Integer.parseInt(sc.nextLine());
+		for (int i=0;i<n;i++)
+		{
+			List<String> request1 = new ArrayList<>();
+			System.out.println("What are the semi optional params?(in single line with spaces)");
+			String[] params = sc.nextLine().split(" ");
+			for(String j:params) request1.add(j);
+			semiOptional.add(request1);
+		}
+		System.out.println("What are the request params given?(in single line with spaces)");
 		String[] params = sc.nextLine().split(" ");
-		for(String i:params) request.add(i);
-		doValidateParams(request,mandatory,optional,semiOptional);
+		for(String j:params) request.add(j);
+		doSemiValidateParams(request,semiOptional);
 		sc.close();
 	}
 }
